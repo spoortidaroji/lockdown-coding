bf5f928221b46ace6adb7127ff58f6fb23512f77
Spoorti S Daroji <43513480+spoortidaroji@users.noreply.github.com>
2020-06-05 13:12:31 +0530

    Add files via upload

---
+++ SumOf3integer.java
@@ -0,0 +1,19 @@
+import java.io.*
+import java.util.Scanner;
+public class SumOf3integer
+{
+	public static void main(String[] args)
+	{       
+		int i,n=0,s=0;
+		{
+	        	System.out.println("Input the 3 numbers: ");  			   
+		}
+		for (i=0;i<3;i++)
+		{
+			Scanner in = new Scanner(System.in);
+			n = in.nextInt();
+  			s +=n;
+		}
+		System.out.println("The sum of 5 no is : " +s);   
+	}
+}
